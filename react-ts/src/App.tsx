31fc832ff4f1ac11d47898d6cb80c7d21c59de79
Izabela Majcherczyk <xeclipse.dev@gmail.com>
2026-03-30 11:55:06 +0200

    config: react + vite + tailwind

---
+++ react-ts/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <h1 className='3xl text-red-400'>Hello</h1>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
