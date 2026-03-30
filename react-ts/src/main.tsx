ef48b423339f710fc049d01a9ff07bbfc24cd61d
Izabela Majcherczyk <xeclipse.dev@gmail.com>
2026-03-30 13:10:33 +0200

    react-feat: working buttons and view

--- react-ts/src/main.tsx
+++ react-ts/src/main.tsx
@@ -1,11 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
 import './styles.css'
 
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <BrowserRouter>
     <App />
-  </StrictMode>,
+  </BrowserRouter>,
 )
